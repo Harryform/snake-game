23f35a1d3a436a8b337b95779363f93428b2132b
Harrison Bush <harryform@Harrisons-MacBook-Pro.local>
2022-03-20 04:32:24 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,73 @@
+from turtle import Screen
+import time
+from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("That Snake Game")
+screen.tracer(0)
+
+
+# segments = []
+
+# game_is_on = True
+# snake = Snake()
+# food = Food()
+#
+# screen.listen()
+# screen.onkey(snake.up, "Up")
+# screen.onkey(snake.down, "Down")
+# screen.onkey(snake.left, "Left")
+# screen.onkey(snake.right, "Right")
+
+
+def game():
+    game_is_on = True
+    snake = Snake()
+    food = Food()
+    scoreboard = Scoreboard()
+
+    screen.listen()
+    screen.onkey(snake.up, "Up")
+    screen.onkey(snake.down, "Down")
+    screen.onkey(snake.left, "Left")
+    screen.onkey(snake.right, "Right")
+
+    while game_is_on:
+
+        screen.update()
+        time.sleep(0.1)
+
+        snake.move()
+
+        # detect collision with food
+        if snake.head.distance(food) < 15:
+            food.refresh()
+            snake.extend()
+            scoreboard.increase_score()
+
+        if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+            scoreboard.reset()
+            snake.reset()
+
+        # detect collision with tail.
+        for segment in snake.segments:
+            if segment == snake.head:
+                pass
+            elif snake.head.distance(segment) < 10:
+                scoreboard.reset()
+                snake.reset()
+
+
+game()
+
+
+new_game = screen.textinput(title="New game?", prompt="Would you like to play a new game? ")
+if new_game == 'y':
+    #  = True
+    screen.reset()
+    game()
+screen.exitonclick()
